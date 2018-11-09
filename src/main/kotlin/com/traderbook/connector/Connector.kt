3fa187410cbf0640bd0c3a7acd8dc104458d8a34
traderbook <contact@traderbook.net>
2018-11-09 15:02:03 +0100

    Mise à jour du connecteur LMAX
    * Permet l'authentification d'un compte de trading
    * Permet la déconnexion d'un compte de trading
    * Retourne un model de type BrokerAccount avec les informations principales du compte (accountId, username et currency)

--- src/main/kotlin/com/traderbook/connector/Connector.kt
+++ src/main/kotlin/com/traderbook/connector/Connector.kt
@@ -1,28 +1,37 @@
 package com.traderbook.connector
 
+import com.lmax.api.LmaxApi
+import com.lmax.api.account.LoginRequest
+import com.lmax.api.account.LoginRequest.ProductType
 import com.traderbook.api.AccountType
 import com.traderbook.api.enums.Messages
 import com.traderbook.api.interfaces.IConnector
 import com.traderbook.api.interfaces.IConnectorObserver
 
-class Connector: IConnector, IConnectorObserver {
-    override fun update(message: Messages, data: Any?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+class Connector(private var connector: IConnectorObserver): IConnector, IConnectorObserver {
+    private var lmax: LmaxApi? = null
+    private var loginRequest: LoginRequest? = null
+    private val customLoginCallback = CustomLoginCallback(this)
 
-    override fun getName(): String {
-        return "Lmax Exchange"
-    }
-    
     override fun connection(accountType: AccountType, username: String, password: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val url = if (accountType == AccountType.DEMO) "https://web-order.london-demo.lmax.com/" else ""
+        val productType = if (accountType == AccountType.DEMO) ProductType.CFD_DEMO else ProductType.CFD_LIVE
+
+        lmax = LmaxApi(url)
+        loginRequest = LoginRequest(username, password, productType)
     }
 
     override fun start() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        lmax!!.login(loginRequest, customLoginCallback)
     }
 
     override fun stop() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        customLoginCallback.logout()
     }
+
+    override fun update(message: Messages, data: Any?) {
+        println(message)
+        connector.update(message, data)
+    }
+
 }
---
+++ src/main/kotlin/com/traderbook/connector/CustomLoginCallback.kt
@@ -0,0 +1,51 @@
+package com.traderbook.connector
+
+import com.lmax.api.FailureResponse
+import com.lmax.api.Session
+import com.lmax.api.account.LoginCallback
+import com.traderbook.api.enums.Instruments
+import com.traderbook.api.enums.Messages
+import com.traderbook.api.models.BrokerAccount
+import kotlin.concurrent.thread
+
+class CustomLoginCallback(private val connector: Connector) : LoginCallback {
+    private var session: Session? = null
+
+    /**
+     * Permet la déconnexion d'un compte chez le broker.
+     * Les informations du compte sont envoyé afin de renseigné l'application quel compte de trading est déconnecté
+     * par le biais de la propriété accountId
+     */
+    fun logout() {
+        val brokerAccount = BrokerAccount(
+                this.session!!.accountDetails.username,
+                this.session!!.accountDetails.accountId.toString(),
+                this.session!!.accountDetails.currency
+        )
+
+        this.session!!.logout(CustomLogoutCallback(connector, brokerAccount))
+    }
+
+    /**
+     * Permet d'intercepter le succès d'authentification
+     */
+    override fun onLoginSuccess(session: Session) {
+        this.session = session
+
+        connector.update(Messages.SUCCESS_LOGIN, BrokerAccount(
+                session.accountDetails.username,
+                session.accountDetails.accountId.toString(),
+                session.accountDetails.currency
+        ))
+
+        thread { this.session!!.start() }
+    }
+
+    /**
+     * Permet d'intercepter l'erreur d'authentification
+     */
+    override fun onLoginFailure(failureResponse: FailureResponse?) {
+        println(failureResponse!!.message)
+        connector.update(Messages.BAD_CREDENTIALS, null)
+    }
+}
