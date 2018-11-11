1162faa082748d741a0925d96da66bfd6319eedb
traderbook <contact@traderbook.net>
2018-11-11 10:04:32 +0100

    Permet la souscription d'un instrument
    * Souscrit une liste défini d'indicateurs
    * Retourne cette liste dès qu'une mise à jour de prix est effectué

--- src/main/kotlin/com/traderbook/connector/CustomLoginCallback.kt
+++ src/main/kotlin/com/traderbook/connector/CustomLoginCallback.kt
@@ -3,13 +3,28 @@ package com.traderbook.connector
 import com.lmax.api.FailureResponse
 import com.lmax.api.Session
 import com.lmax.api.account.LoginCallback
+import com.lmax.api.orderbook.OrderBookEventListener
+import com.lmax.api.orderbook.OrderBookSubscriptionRequest
 import com.traderbook.api.enums.Instruments
 import com.traderbook.api.enums.Messages
 import com.traderbook.api.models.BrokerAccount
+import com.traderbook.api.models.Instrument
+import com.traderbook.api.models.InstrumentCollection
 import kotlin.concurrent.thread
 
 class CustomLoginCallback(private val connector: Connector) : LoginCallback {
+    private val instrumentsRef = mapOf(
+            Instruments.EURUSD to 4001L,
+            Instruments.GBPUSD to 4002L
+    )
+
+    private val instruments = mutableMapOf<Long, Instrument>()
+
     private var session: Session? = null
+
+    init {
+        instrumentsRef.forEach { instruments[it.value] = Instrument(it.key, 0.0, 0.0, 0.0, 0.0) }
+    }
 
     /**
      * Permet la déconnexion d'un compte chez le broker.
@@ -33,19 +48,36 @@ class CustomLoginCallback(private val connector: Connector) : LoginCallback {
         this.session = session
 
         connector.update(Messages.SUCCESS_LOGIN, BrokerAccount(
-                session.accountDetails.username,
-                session.accountDetails.accountId.toString(),
-                session.accountDetails.currency
+                this.session!!.accountDetails.username,
+                this.session!!.accountDetails.accountId.toString(),
+                this.session!!.accountDetails.currency
         ))
 
-        thread { this.session!!.start() }
+        this.session!!.registerOrderBookEventListener {
+            instruments[it.instrumentId]?.oldAsk = instruments[it.instrumentId]!!.ask
+            instruments[it.instrumentId]?.ask = it.valuationAskPrice.toString().toDouble()
+            instruments[it.instrumentId]?.oldBid = instruments[it.instrumentId]!!.bid
+            instruments[it.instrumentId]?.bid = it.valuationBidPrice.toString().toDouble()
+
+            connector.update(Messages.INSTRUMENTS_UPDATED, InstrumentCollection(instruments))
+        }
+
+        instruments.forEach { this.session!!.subscribe(OrderBookSubscriptionRequest(it.key), CustomSubscriptionCallback()) }
+
+        thread {
+            try {
+                this.session!!.start()
+            } catch (e: NullPointerException) {
+                connector.update(Messages.BROKER_MAINTENANCE_MODE, null)
+            }
+        }
     }
 
     /**
      * Permet d'intercepter l'erreur d'authentification
      */
     override fun onLoginFailure(failureResponse: FailureResponse?) {
-        if(failureResponse!!.message == "Invalid character found in beginning of XML comment: 'D'") {
+        if (failureResponse!!.message == "Invalid character found in beginning of XML comment: 'D'") {
             connector.update(Messages.BROKER_MAINTENANCE_MODE, null)
         } else {
             connector.update(Messages.BAD_CREDENTIALS, null)
